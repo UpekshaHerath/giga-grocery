f27dbf531969d3cb9c275f4a12882070ea7b8014
Upeksha-Herath <upeksha.herath@rootcodelabs.com>
2024-01-28 22:46:21 +0530

    begin speing security

---
+++ backend/src/main/java/com/inventoryManagement/backend/configurations/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.inventoryManagement.backend.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    // Authentication : User --> Roles
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+        UserDetails admin = User.withUsername("Upeksha")
+                .password(encoder.encode("1234"))
+                .roles("ADMIN")
+                .build();
+        UserDetails user = User.withUsername("Herath")
+                .password(encoder.encode("1234"))
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+
+
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("product/products").permitAll()
+                            .requestMatchers("/product/**").authenticated();
+                }).build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
